6ac8aa827738dcf16d79ab41d414cd925f81e344
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-10-30 18:51:02 -0400

    Add comments to ocr.py
    #10

--- ocr.py
+++ ocr.py
@@ -7,6 +7,7 @@ import json
 invert = True
 threshold = 200
 
+# List of file names for testing
 #filename = 'Annotation 2019-10-19 160040.png'
 filename = 'testimg1.png'
 #filename = 'testimg2.jpg'
@@ -17,13 +18,17 @@ filename = 'unknown.png'
 filename = 'u.png'
 print('Analyzing %s...'%filename)
 
+# Open file and convert to 3 channels
 img = Image.open(filename)
 img = img.convert('RGB')
+
 #blackwhite = gray.point(lambda x: 0 if x < 200 else 255, '1')
 
 #stat = ImageStat.Stat(im)
 #avg = stat.mean[0]
 
+# Get average brightness from image
+# TODO: add cite
 def brightness(image):
 	grayscale = image.convert('L')
 	histogram = grayscale.histogram()
@@ -36,14 +41,16 @@ def brightness(image):
 		
 	return b / scale
 
-
+# Apply binarization (reduce image to )
 img = img.point(lambda x: 0 if x < threshold else 255)
 lightness = brightness(img)
 print('Brightness averages %s across image'%brightness(img))
+# If invert setting is enabled and binarized image brightness is low, invert the image so the light text becomes dark
 if lightness < 0.5 and invert:
 	print('Inverting image before processing...')
 	img = ImageOps.invert(img)
-	
+
+# Analyze image and print OCR'd text
 print('Detected text: \n')
 text = pytesseract.image_to_string(img);
 
