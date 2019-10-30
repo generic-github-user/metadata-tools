f8eba62fd16bff9925a21492f20c876c112581cd
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-10-29 22:18:49 -0400

    File names for testing
    gotta fill up that contrib graph

--- ocr.py
+++ ocr.py
@@ -3,3 +3,14 @@ from PIL import Image, ImageOps
 import pytesseract
 import json
 #import ImageStat
+
+invert = True
+
+#filename = 'Annotation 2019-10-19 160040.png'
+filename = 'testimg1.png'
+#filename = 'testimg2.jpg'
+#filename = 'testimg3.png'
+#filename = 'testimg4.png'
+#filename = 'testimg5.png'
+filename = 'unknown.png'
+print('Analyzing %s...'%filename)
